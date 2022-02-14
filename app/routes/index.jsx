043bc298d1e726d21ad5194a1ed0627809f60609
Jinson PG <jinsongeorge62@gmail.com>
2022-02-14 23:04:36 +0530

    Project created

---
+++ app/routes/index.jsx
@@ -0,0 +1,11 @@
+export default function Index() {
+  return (
+    <div className="full">
+      <div className="firstdiv">
+ <text className="carehead">
+   Care Helpers
+ </text>
+ </div>
+    </div>
+  );
+}
